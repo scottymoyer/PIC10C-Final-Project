b1775091a96f18137dc318496f3a8ba36be4b6c6
Scott <scottymoy@gmail.com>
2017-12-16 09:49:32 -0800

    Update main.cpp
    main routine

--- main.cpp
+++ main.cpp
@@ -1,79 +1,70 @@
 //
 //  main.cpp
-//  Battleship
+//  BattleshipFinalProject
 //
-//  Created by Scott Moyer on 11/7/17.
+//  Created by Scott Moyer on 12/02/17.
 //  Copyright © 2017 Scott Moyer. All rights reserved.
 //
 
+#include "game.h"
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
 #include <string>
 #include <vector>
-#include "game.h"
 
-
-
-int main(){
+int main() {
+    //Prompts user to choose game settings at beginning of game
     srand(int(time(0)));
     std::cout<<"Welcome to Battleship!\n\n";
+    std::cout<<"In this version of Battleship, you can choose the number of rows, columns, and ships.\n";
+    std::cout<<"The default game settings are a 5x5 board and 5 battleships.\n";
     std::cout<<"Would you like to choose your own game settings?\nEnter 'y' or 'n': ";
     char ans;
     int r,c,s;
     std::cin>>ans;
-    cin.ignore();
+    std::cin.ignore();
     if(ans=='y'){
-        std::cout<<"Okay. How many rows would you like? ";
+        std::cout<<"Okay. How many rows would you like? (Up to 10) ";
         std::cin>>r;
-        cin.ignore();
-        std::cout<<"How many columns? ";
+        std::cin.ignore();
+        std::cout<<"How many columns? (Up to 10) ";
         std::cin>>c;
-        cin.ignore();
-        std::cout<<"How many battleships? ";
+        std::cin.ignore();
+        std::cout<<"How many battleships? (Up to 10) ";
         std::cin>>s;
-        cin.ignore();
+        std::cin.ignore();
+        while(s > r*c || s > 10){
+            std::cout<<"That's too many battleships. How many battleships would you like? (Up to 10) ";
+            std::cin>>s;
+            std::cin.ignore();
+        }
         std::cout<<std::endl;
     }
     else {
-        std::cout<<"The default game settings are a 5x5 board and 5 battleships.\n\n";
         r=5;
         c=5;
         s=5;
     }
-    Board::Board b(r,c,s);
     
-    //Fix set up
+    //Creates attack and defense boards
+    AttackBoard::AttackBoard attackb(r,c,s);
+    DefenseBoard::DefenseBoard defenseb(r,c,s);
     
-    for(int i = 0; i<b.getnumships(); i++){
-        int xcoord, ycoord;
-        std::cout<<"Enter the X-coordinate for ship #"<< (i+1) << ": ";
-        std::cin>>xcoord;
-        std::cin.ignore();
-        std::cout<<"Enter the Y-coordinate for ship #"<< (i+1) << ": ";
-        std::cin>>ycoord;
-        std::cin.ignore();
-        std::cout<<std::endl;
-
-        
-        if(b.notvalidcoordinate(xcoord, ycoord)){
-            std::cout<<"Please enter valid coordinates for this ship.\n";
-            i-=1;
-            continue;
-        }
-        if(b.getdefensevalue(xcoord,ycoord)>0){
-            std::cout<<"Please enter different coordinates for this ship.\n";
-            i-=1;
-            continue;
-        }
-           b.setships(xcoord, ycoord, i);
-    }
+    //Creates base class pointers to use throughout program
+    Board::Board *ab = &attackb;
+    Board::Board *db = &defenseb;
     
-    b.printattack();
-    b.printdefense();
+    //Set ships
+    ab->setships();
+    db->setships();
     
-    while(b.getmyhealth()>0 && b.getenemyhealth()>0){
-        
+    ab->printboard();
+    db->printboard();
+    
+    //Continue game until one player runs out of health
+    while(ab->gethealth()>0 && db->gethealth()>0){
+        //Prompts user to enter attack coordinates
         int xattackcoord, yattackcoord;
         std::cout<<std::endl<<"Enter attack X-coordinate: ";
         std::cin>>xattackcoord;
@@ -82,57 +73,64 @@ int main(){
         std::cin>>yattackcoord;
         std::cin.ignore();
         std::cout<<std::endl;
-        if(b.notvalidcoordinate(xattackcoord, yattackcoord)){
+        if(ab->notvalidcoordinate(xattackcoord, yattackcoord)){
             std::cout<<"Please enter valid attack coordinates.\n";
             continue;
         }
-        else if(b.getattackvalue(xattackcoord, yattackcoord)%4>1){
+        else if(ab->getvalue(xattackcoord, yattackcoord)%4>1){
             std::cout<<"You have already fired at these coordinates. Please choose again.\n";
             continue;
         }
         
-        b.attack(xattackcoord, yattackcoord);
-        if(b.getattackvalue(xattackcoord, yattackcoord)==2){
+        //Fires and displays result
+        ab->fire(xattackcoord, yattackcoord);
+        
+        if(ab->getvalue(xattackcoord, yattackcoord)==2){
             std::cout<<"You missed!\n\n";
         }
-        else if(b.getattackvalue(xattackcoord, yattackcoord)%4==3){
-            std::cout<<"Hit! Target acquired.\nYou sunk the enemy's " << b.getattackship(xattackcoord, yattackcoord)<< ".\n";
-            b.minusenemyhealth();
-            std::cout<<"Enemy has "<<b.getenemyhealth()<<" ships remaining.\n\n";
+        else if(ab->getvalue(xattackcoord, yattackcoord)%4==3){
+            std::cout<<"Hit! Target acquired.\nYou sunk the enemy's " << ab->getshipname(xattackcoord, yattackcoord)<< ".\n";
+            ab->minushealth();
+            std::cout<<"Enemy has "<<ab->gethealth()<<" ships remaining.\n\n";
         }
-        b.printattack();
+        ab->printboard();
         
+        //Enemy generates random coordinates and fires
         int xdefensecoord, ydefensecoord;
-        xdefensecoord = 1 + rand() % b.getnumcols();
-        ydefensecoord = 1 + rand() % b.getnumrows();
-        while(b.getdefensevalue(xdefensecoord, ydefensecoord)%4>1){
-            xdefensecoord = 1 + rand() % b.getnumcols();
-            ydefensecoord = 1 + rand() % b.getnumrows();
+        xdefensecoord = 1 + rand() % db->getnumcols();
+        ydefensecoord = 1 + rand() % db->getnumrows();
+        while(db->getvalue(xdefensecoord, ydefensecoord)%4>1){
+            xdefensecoord = 1 + rand() % db->getnumcols();
+            ydefensecoord = 1 + rand() % db->getnumrows();
         }
-        b.defend(xdefensecoord,ydefensecoord);
+        db->fire(xdefensecoord,ydefensecoord);
         
+        //Displays result of fire
         std::cout<<"Enemy fired at ("<<xdefensecoord<<", "<<ydefensecoord<<").\n";
-        if(b.getdefensevalue(xdefensecoord, ydefensecoord)==2){
+        if(db->getvalue(xdefensecoord, ydefensecoord)==2){
             std::cout<<"They missed!\n\n";
         }
-        else if(b.getdefensevalue(xdefensecoord, ydefensecoord)%4==3){
-            std::cout<<"You've been hit!\nThe enemy sunk your " << b.getdefenseship(xdefensecoord, ydefensecoord)<<".\n";
-            b.minusmyhealth();
-            std::cout<<"You have "<<b.getmyhealth()<<" ships remaining.\n\n";
+        else if(db->getvalue(xdefensecoord, ydefensecoord)%4==3){
+            std::cout<<"You've been hit!\nThe enemy sunk your " << db->getshipname(xdefensecoord, ydefensecoord)<<".\n";
+            db->minushealth();
+            std::cout<<"You have "<<db->gethealth()<<" ships remaining.\n\n";
         }
-        b.printdefense();
- 
+        db->printboard();
+        
     }
     
-    if(b.getmyhealth()==0 && b.getenemyhealth()==0){
+    //Different possible outcomes
+    if(ab->gethealth()==0 && ab->gethealth()==0){
         std::cout<<"Tie! You both sunk all of each other's ships!\n";
     }
-    else if(b.getmyhealth()==0){
+    else if(db->gethealth()==0){
         std::cout<<"You lose! The enemy sunk all of your ships!\n";
     }
-    else if(b.getenemyhealth()==0){
+    else if(ab->gethealth()==0){
         std::cout<<"You win! You sunk all of your enemy's ships!\n";
     }
+    
+    
 
     return 0;
 }
